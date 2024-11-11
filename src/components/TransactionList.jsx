142c18374201a91ad83b415ff48e28a2ed3b20b0
Desktop <Aditya>
2024-11-11 21:35:41 +0530

    updates

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import {Transaction} from './Transaction'
 const TransactionList = () => {
-  return (
-    <div><h3>History</h3>
-    <ul id="list" className="list">
-       <li className="minus">
-        Cash <span>-$400</span><button className="delete-btn">x</button>
-        {/* Cash <span>+$200</span><button className="delete-btn">x</button> */}
+    const { transactions } = useContext(GlobalContext);
+    console.log(transactions)
+    return (
+        <>
+            <h3>History</h3>
+            <ul className="list">
+                {transactions.map((transaction) => (
+                    <Transaction key={transaction.id} transaction={transaction}/>
+                ))}
+            </ul>
+        </>
+    );
+};
 
-      </li> 
-    </ul></div>
-  )
-}
-
-export default TransactionList
+export default TransactionList;
--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ExpenseTracker from './components/Header.jsx'
+import Header from './components/Header.jsx'
 import Balance from './components/Balance.jsx'
 import IncomeExpenses from './components/IncomeExpenses.jsx'
 import TransactionList from './components/TransactionList.jsx'
 import AddTransaction from './components/AddTransaction.jsx'
-
+import { GlobalProvider } from './context/GlobalState.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <GlobalProvider>
     <App />
-    <ExpenseTracker />
+    <Header />
     <div className="container">
       <Balance />
       <IncomeExpenses />
       <TransactionList  />
       <AddTransaction />
       </div>
-  </StrictMode>,
+  </GlobalProvider>,
 )
